4469becd8994b53217117c3b594619b791489d4f
Azaruddin8297 <ajarpopo.com>
2023-03-29 10:04:25 +0530

    StackandQueue

---
+++ StackandQueue/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace StackandQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StackList list = new StackList();
+            list.Push(10);
+            list.Push(20);
+            list.Push(30);
+            list.Push(50);
+            Console.WriteLine("*************************");
+            list.Display();
+        }
+    }
+}
